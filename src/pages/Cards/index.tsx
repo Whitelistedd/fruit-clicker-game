03a6e8e246d5feadfe7d7b3c3b068bb2f910cd24
whitelistedd <geforcexxy@gmail.com>
2024-09-10 13:11:50 +0300

    edit Cards, Friends

--- src/pages/Cards/index.tsx
+++ src/pages/Cards/index.tsx
@@ -1,12 +1,126 @@
-import { List } from '@telegram-apps/telegram-ui';
+import {List, Section} from '@telegram-apps/telegram-ui';
 
-import type { FC } from 'react';
+import {FC, useEffect, useState} from 'react';
+import styles from "@/pages/Cards/Cards.module.scss";
+import {InfoPill} from "@/components/InfoPill";
+import {formatNumber} from "@/helpers/formatNumber.ts";
+import {useAppDispatch, useAppSelector} from "@/store";
+import Coin from "@/assets/svgs/Coin.svg?react";
+import {useGetFruitsQuery} from "@/store/slices/fruits";
+import {FruitCard} from "@/components/FruitCard";
+import {ActionModal} from "@/components/ActionModal/Index.tsx";
+import {setMainFruit, setUserFruitLevels} from "@/store/slices/user";
+import {fruitType} from "@/store/slices/fruits/fruits.types.ts";
 
 export const Cards: FC = () => {
+  const {
+    per_tap,
+    total_taps_counter,
+    main_fruit,
+    user_fruit_levels
+  } = useAppSelector((state) => state.user);
+  const [actionModalType, setActionModalType] = useState<{type: "purchase" | "activate", fruitId: number}>({type: "purchase", fruitId: 0})
+
+  const {data, refetch} = useGetFruitsQuery({})
+    const dispatch = useAppDispatch()
+
+    const handleBuyFruit = () => {
+        const newUserFruitLevels = [...user_fruit_levels]
+        newUserFruitLevels.push({
+            created_at: "",
+            current: 0,
+            fruit_id: actionModalType.fruitId,
+            level: 1,
+            taps: 0,
+            unlocked: true,
+            user_id: "",
+        },)
+        dispatch(setUserFruitLevels(newUserFruitLevels))
+        toggleBuyFruitModal()
+    }
+
+    const handleActivateFruit = () => {
+      if(!data) return
+      const newFruit = data.find(fruit => fruit.id ==  actionModalType.fruitId) as fruitType
+      dispatch(setMainFruit(newFruit))
+        toggleActivateFruitModal()
+    }
+
+    const toggleBuyFruitModal = (fruitId?: number) => {
+        setActionModalType({type:"purchase", fruitId: fruitId || 0 })
+    }
+
+    const toggleActivateFruitModal = (fruitId?: number) => {
+        setActionModalType({type:"activate", fruitId: fruitId || 0 })
+    }
+
+    useEffect(() => {
+        refetch()
+    }, []);
 
   return (
     <List>
-      <></>
+        <Section>
+            {Number(actionModalType.fruitId) > 0 &&
+                <ActionModal
+                    title={actionModalType.type === "purchase" ? "Are you sure you want to Purchase this Fruit?" : "Are you sure you want to Activate this Fruit"}
+                    toggleModal={actionModalType.type === "purchase" ? toggleBuyFruitModal : toggleActivateFruitModal}
+                    onSubmit={actionModalType.type === "purchase" ? handleBuyFruit : handleActivateFruit}
+                />
+            }
+            <div className={styles.container}>
+                <div className={styles.wrap}>
+                    <div className={styles.infoPills}>
+                        <div className={styles.topInfo}>
+                            <InfoPill
+                                label="Balance"
+                                className={styles.totalCoinsCounter}
+                                wrapClassName={`${styles.totalCoinsCounterWrap} ${styles.infoCountPill}`}
+                            >
+                                <Coin/>
+                                <span className={`${styles.totalCoinsCount} ${styles.infoCount}`}>
+                              {Number(total_taps_counter).toLocaleString()}
+                            </span>
+                            </InfoPill>
+                            <InfoPill wrapClassName={styles.infoCountPill} label="Per hour">
+                                <Coin/>
+                                <span className={styles.infoCount}>
+                                {formatNumber(per_tap * 60)}
+                            </span>
+                            </InfoPill>
+                        </div>
+                    </div>
+                    <div className={styles.cards}>
+                        {
+                            data?.map(fruit => {
+                                const fruitUserStats = user_fruit_levels.find(fruitStats => fruitStats.fruit_id === fruit.id)
+                                const per_tap = fruit?.levels && (fruitUserStats?.level ? fruit.levels[fruitUserStats?.level as keyof typeof fruit.levels].taps_per_tap : fruit.levels[1].taps_per_tap)
+                                return (
+                                    <FruitCard
+                                        fruitId={fruit.id}
+                                        onPurchase={
+                                            !fruitUserStats?.unlocked ?
+                                                () => toggleBuyFruitModal(fruit.id)
+                                            : fruit.id !== main_fruit?.id ?
+                                                () => toggleActivateFruitModal(fruit.id)
+                                            : undefined}
+                                        key={fruit.id}
+                                        price={fruit.price || 100}
+                                        name={fruit.name || "-"}
+                                        color={fruit?.color || undefined}
+                                        fruitImg={fruit.src || ""}
+                                        active={fruit.id === main_fruit?.id}
+                                        perHour={(per_tap || 1) * 60}
+                                        unlocked={fruitUserStats?.unlocked || false}
+                                        level={fruitUserStats?.level || "0"}
+                                    />
+                                )
+                            })
+                        }
+                    </div>
+                </div>
+            </div>
+        </Section>
     </List>
   );
 };
--- src/pages/Friends/index.tsx
+++ src/pages/Friends/index.tsx
@@ -1,12 +1,17 @@
-import { List } from '@telegram-apps/telegram-ui';
+import {List, Section} from '@telegram-apps/telegram-ui';
 
 import type { FC } from 'react';
+import styles from "@/pages/IndexPage/IndexPage.module.scss";
 
 export const Friends: FC = () => {
 
   return (
     <List>
-      <></>
+        <Section>
+            <div className={styles.container}>
+                Soon.
+            </div>
+        </Section>
     </List>
   );
 };
